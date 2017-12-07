028ae61fff51e0e993c486e1b0c24de9a176c476
Amanda Gonzalez <gonzalez.a1@ufl.edu>
2017-12-07 11:08:16 -0500

    Methods created & actions assigned to defenders

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -1,11 +1,10 @@
-
 package ufl.cs1.controllers;
 
-		import game.controllers.DefenderController;
-		import game.models.*;
-		import game.system.*;
+import game.controllers.DefenderController;
+import game.models.*;
+import game.system.*;
 
-		import java.util.List;
+import java.util.List;
 
 public final class StudentController implements DefenderController
 {
@@ -17,54 +16,47 @@ public final class StudentController implements DefenderController
 	{
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
+		Attacker attacker = game.getAttacker();
 
-		Attacker attacker = game.getAttacker(); //check if i can do this
+		for(int i = 0; i < actions.length; i++) {
 
-		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
-		//any random number of all of the ghosts
-		for(int i = 0; i < actions.length; i++)
-		{
 			Defender defender = enemies.get(i);
 			List<Integer> possibleDirs = defender.getPossibleDirs();
 
-			if (possibleDirs.size() != 0){
+			if (possibleDirs.size() != 0) {
 
-				/*********************************ERASE THIS CODE**********************************************
-				 /*List<Node> possibleLocations = attacker.getPossibleLocations(true); //possible nodes A could move to
-				 Node targetNode = defender.getTargetNode(possibleLocations,true); //find closest possible location of A from D
-				 //Check documentation // This^ might send the D to an empty location
+				if(i % 2 == 0)
+					actions[i] = defenderChase(attacker, defender);
 
-				 if (!defender.isVulnerable()) //if not blue
-				 actions[i] = defender.getNextDir(targetNode, true);
-				 *********************************************************************************************/
+				//else
+				//actions[i] = defenderAmbush();
 
-				int nextDir = defender.getNextDir(attacker.getLocation(),true);
-				int nextDir2 = defender.getNextDir(attacker.getLocation(), false);
-
-				if (!defender.isVulnerable()){
-					if(defender.getLocation().getNeighbor(nextDir).isPill()) //check if not a wall or vulnerable
-						actions[i] = nextDir;
-
-					//else
-					//code to switch directions if there is a wall
-
-				}
-
-				else if (defender.isVulnerable()) {
-					if(defender.getLocation().getNeighbor(nextDir2).isPill())
-						actions[i] = nextDir2;
-					//actions[i] = defender.getNextDir(targetNode, false);
-
-					//else
-					//code to switch directions if there is a wall
-				}
 			}
-
-			//actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
 
 			else
 				actions[i] = -1;
 		}
 		return actions;
 	}
+
+	public int defenderChase(Attacker attacker, Defender defender){
+
+		int nextDir = defender.getNextDir(attacker.getLocation(),true);
+		int nextDir2 = defender.getNextDir(attacker.getLocation(), false);
+
+		if (!defender.isVulnerable() && defender.getLocation().getNeighbor(nextDir).isPill()) {
+
+			return nextDir;
+		}
+
+		else if (defender.isVulnerable() && defender.getLocation().getNeighbor(nextDir2).isPill()) {
+
+			return nextDir2;
+		}
+
+		return -1;
+	}
+
+	//public int defenderAmbush(){	}
+
 }
