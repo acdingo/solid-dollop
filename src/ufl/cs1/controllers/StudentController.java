fe7a1c1c4956352da2482ebf557cf59c046e8c0d
acdingo <adainigo@gmail.com>
2017-12-07 20:06:22 -0500

    With something for ambush using list node and base of chaser

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -6,50 +6,46 @@ import game.system.*;
 
 import java.util.List;
 
-public final class StudentController implements DefenderController
-{
-	public void init(Game game) { }
+public final class StudentController implements DefenderController {
+	public void init(Game game) {
+	}
 
-	public void shutdown(Game game) { }
+	public void shutdown(Game game) {
+	}
 
-	public int[] update(Game game,long timeDue)
-	{
+	public int[] update(Game game, long timeDue) {
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
 		Attacker attacker = game.getAttacker();
 
-		for(int i = 0; i < actions.length; i++) {
+		for (int i = 0; i < actions.length; i++) {
 
 			Defender defender = enemies.get(i);
 			List<Integer> possibleDirs = defender.getPossibleDirs();
 
 			if (possibleDirs.size() != 0) {
 
-				if(i % 2 == 0)
+				if (i % 2 == 0)
 					actions[i] = defenderChase(attacker, defender);
 
-				//else
-				//actions[i] = defenderAmbush();
+				else
+					actions[i] = defenderAmbush(attacker, defender);
 
-			}
-
-			else
+			} else
 				actions[i] = -1;
 		}
 		return actions;
 	}
 
-	public int defenderChase(Attacker attacker, Defender defender){
+	public int defenderChase(Attacker attacker, Defender defender) {
 
-		int nextDir = defender.getNextDir(attacker.getLocation(),true);
+		int nextDir = defender.getNextDir(attacker.getLocation(), true);
 		int nextDir2 = defender.getNextDir(attacker.getLocation(), false);
 
 		if (!defender.isVulnerable() && defender.getLocation().getNeighbor(nextDir).isPill()) {
 
 			return nextDir;
-		}
-
-		else if (defender.isVulnerable() && defender.getLocation().getNeighbor(nextDir2).isPill()) {
+		} else if (defender.isVulnerable() && defender.getLocation().getNeighbor(nextDir2).isPill()) {
 
 			return nextDir2;
 		}
@@ -57,6 +53,28 @@ public final class StudentController implements DefenderController
 		return -1;
 	}
 
-	//public int defenderAmbush(){	}
+	public int defenderAmbush(Attacker attacker, Defender defender) {
+		List<Node> maybe = attacker.getPossibleLocations(true);
+		//List<Node> thisthing = attacker.getPossibleLocations(true);
+        int nextDir;
+		try {
+			nextDir = defender.getNextDir(maybe.get(0), true);
+		}
+		catch(Exception e) {
+			nextDir = defender.getNextDir(attacker.getLocation(), true);
+		}
+		int nextDir2 = defender.getNextDir(attacker.getLocation(), false);
 
-}
+
+			if (!defender.isVulnerable() && defender.getLocation().getNeighbor(nextDir).isPill()) {
+
+				return nextDir;
+			} else if (defender.isVulnerable() && defender.getLocation().getNeighbor(nextDir2).isPill()) {
+
+				return nextDir2;
+			}
+
+			return -1;
+	}
+	}
+
